2ee3ffdd2b8888da9fc25be00a82a9c94472d0a4
Anusha Anand <anandanusha610@gmail.com>
2026-04-04 12:39:01 +0530

    added comment explaining date field bug and fix

--- app/services/transaction_service.py
+++ app/services/transaction_service.py
@@ -25,6 +25,12 @@ def get_transactions(db: Session, skip: int = 0, limit: int = 50, type_filter: O
 
 def create_transaction(db: Session, transaction_in: TransactionCreate, user_id: int):
     create_data = transaction_in.model_dump(exclude_unset=True)
+    # If date is passed as None or omitted, we remove it to let SQLAlchemy's default kick in
+    '''
+    So earlier here, I encountered a mismatch where the API schema allowed a nullable date field, but the database enforced it as non-nullable, 
+    causing the transaction creation to fail which I have resolved now by using a default_factory in the Pydantic schema to automatically assign the current date. 
+    This is now consistent with the database too.
+    '''
     if create_data.get("date") is None:
         create_data.pop("date", None)
     db_transaction = Transaction(**create_data, created_by_id=user_id)
